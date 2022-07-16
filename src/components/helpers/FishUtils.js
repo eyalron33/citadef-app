18ef9eeffc029e1d954f903d0b703668f56dd1d4
eyalron33 <eyalron33@gmail.com>
2022-07-16 09:57:29 +0200

    modify frontpage design, improve random order of fishes on frontpage

--- src/components/helpers/FishUtils.js
+++ src/components/helpers/FishUtils.js
@@ -39,3 +39,14 @@ export const fishPageUrl = (seed) => {
 export const postPageUrl = (seed, postId) => {
     return "/personal/post/edit/" + seed + "/" + postId
 }
+
+export const shuffleArray = (sourceArray) => {
+    for (var i = 0; i < sourceArray.length - 1; i++) {
+        var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+
+        var temp = sourceArray[j];
+        sourceArray[j] = sourceArray[i];
+        sourceArray[i] = temp;
+    }
+    return sourceArray;
+}
